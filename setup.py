cc8d2bd4e281153da744323171e1a893d570ba93
PatelKamya <kamyapatel521@gmail.com>
2025-07-05 12:10:17 +0530

    folder structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Generative-AI-Assistant',
+    version='0.0.0',
+    author='kamya',
+    author_email='kamyapatel303@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+)
